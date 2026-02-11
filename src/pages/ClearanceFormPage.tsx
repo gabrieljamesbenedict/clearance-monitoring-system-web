e7e8669fa1fc1a2533746103a726d0992a29f18b
Gabriel James Benedict <benok.loslos@gmail.com>
2026-02-12 07:59:14 +0800

    feat: migrate form page to React

---
+++ src/pages/ClearanceFormPage.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from '../styles/form.module.css'
+
+const ClearanceFormPage = () => {
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+    }
+
+    return (
+    <div className={styles.pageBody}>
+        <h1>Clearance Request Form</h1>
+        <form className={styles.formCard} onSubmit={handleSubmit}>
+            <label className={styles.formCard__label}>Student Name</label>
+            <input className={styles.formCard__input} type="text" required/>
+
+            <label className={styles.formCard__label}>Student Number</label>
+            <input className={styles.formCard__input} type="number" required/>
+
+            <label className={styles.formCard__label}>Program</label>
+            <input className={styles.formCard__input} type="text" required/>
+
+            <label className={styles.formCard__label}>Purpose of Clearance</label>
+            <textarea className={styles.formCard__input} rows={4} required/>
+
+            <input className={styles.formCard__submit} type="submit"/>
+        </form>
+    </div>
+    )
+}
+
+export default ClearanceFormPage
