1a58a7092c1f6c372ed67889eaf63ffa8c5faba5
Gabriel James Benedict <benok.loslos@gmail.com>
2026-02-25 19:18:51 +0800

    feat: add initial top bar

---
+++ src/app/components/NavigationBar.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import mapuaLogo from '@/images/mapua_logo2.png'
+
+const NavigationBar = () => {
+  return (
+    <div className='bg-primary flex px-8 py-4 text-white'>
+      <Image 
+          src={mapuaLogo}
+          alt="Mapua University logo"
+          className='h-16 w-auto'
+        />
+    </div>
+  )
+}
+
+export default NavigationBar
