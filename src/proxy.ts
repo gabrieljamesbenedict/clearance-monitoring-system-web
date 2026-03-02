42577e95f07b0e0a9c2920ecb7b397a737d09bb3
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-02 17:10:06 +0800

    refactor: remove auth checking from proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,33 +1,27 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-import { me, User } from './app/service/AuthService'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import path from 'path';
 
-export function proxy (request: NextRequest) {
-
-    const cookie = request.headers.get("JSESSIONID");
+export function proxy(request: NextRequest) {
+    const cookie = request.cookies.get("JSESSIONID");
     const url = request.nextUrl.clone();
 
     const publicPaths = ["/login", "/register"];
-    const protectedPaths = ["/dashboard", "/form", "/admin"];
-
-    let pathname = request.nextUrl.pathname;
+    const pathname = request.nextUrl.pathname;
 
     if (pathname === "/") {
-        pathname = "/dashboard";
-    }
-
-    if (cookie && publicPaths.some(path => pathname.startsWith(path))) {
         url.pathname = "/dashboard";
         return NextResponse.redirect(url);
     }
 
-    if (!cookie && protectedPaths.some(path => pathname.startsWith(path))) {
-        url.pathname = "/login";
-        return NextResponse.redirect(url);
+    if (publicPaths.some(path => pathname.startsWith(path))) {
+        if (cookie) {
+            url.pathname = "/dashboard";
+            return NextResponse.redirect(url);
+        }
     }
 
     return NextResponse.next();
-
 }
 
 export const config = {
