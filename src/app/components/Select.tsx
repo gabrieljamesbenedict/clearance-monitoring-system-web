e08ab657d51b31e44dbeed4d04f721d82241d47c
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-03 02:18:54 +0800

    feat: add overflow protection and truncation

--- src/app/components/Select.tsx
+++ src/app/components/Select.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Select = ({children, placeholder}: any) => {
+const Select = ({children, placeholder, value, onChange}: any) => {
   return (
-    <select className="border border-gray-400 rounded-2xl px-4 py-2.5 focus:outline-none focus:border-gray-800">
+    <select className="w-full min-w-0 border border-gray-400 rounded-2xl px-4 py-2.5 focus:outline-none focus:border-gray-800 overflow-x-visible truncate"
+      value={value}
+      onChange={onChange}
+    >
         <option value="">{placeholder}</option>
         {children}
     </select>
