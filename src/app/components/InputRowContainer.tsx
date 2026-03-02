bc1aa8009e41ce21967ff6a7641b44eab78e2af2
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-02 19:27:50 +0800

    feat: add InputRowContainer

---
+++ src/app/components/InputRowContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InputRowContainer = ({children}: any) => {
+  return (
+    <div className="w-full flex *:flex-1 gap-2">
+        {children}
+    </div>
+  )
+}
+
+export default InputRowContainer
