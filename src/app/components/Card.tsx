ab7a9c6e5ee75f24bb563071a9d612b450279573
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-02 19:27:39 +0800

    style: add more shadow to Card

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 const Card = ({ children}: any) => {
   return (
-    <div
-      className="min-w-[30%] p-4 mx-auto my-16 bg-background-card rounded-xl shadow-2xs"
-    >
+    <div className="min-w-[30%] p-4 mx-auto my-16 bg-background-card rounded-xl shadow-xl">
       {children}
     </div>
   );
