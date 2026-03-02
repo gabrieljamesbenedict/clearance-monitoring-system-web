12a7e9b546cc0ec1a53d1e85e0cef44aa3dd19d8
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-02 20:45:03 +0800

    feat: add ProgramService

---
+++ src/app/service/ProgramService.ts
@@ -0,0 +1,33 @@
+export interface Program {
+    programId: number;
+    name: string;
+    description: string;
+}
+
+export const getAll = async (): Promise<Program[]> => {
+    const res = await fetch(`http://localhost:8080/api/programs`, {
+        method: "GET",
+        credentials: "include",
+        headers: {"Content-Type": "application/json"}
+    });
+
+    if (!res.ok) {
+        throw new Error(`Request failed: ${res.status}`);
+    }
+
+    return await res.json();
+}
+
+export const get = async (id: number): Promise<Program> => {
+    const res = await fetch(`http://localhost:8080/api/programs/${id}`, {
+        method: "GET",
+        credentials: "include",
+        headers: {"Content-Type": "application/json"}
+    });
+
+    if (!res.ok) {
+        throw new Error(`Request failed: ${res.status}`);
+    }
+
+    return await res.json();
+}
