b6677af61f1a626c13b6ccccda859a4f8f7cf00e
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-05 01:47:15 +0800

    chore: organize code

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -5,10 +5,10 @@ const NavigationBar = () => {
   return (
     <div className='bg-primary flex px-8 py-4 text-white'>
       <Image 
-          src={mapuaLogo}
-          alt="Mapua University logo"
-          className='h-16 w-auto'
-        />
+        src={mapuaLogo}
+        alt="Mapua University logo"
+        className='h-16 w-auto'
+      />
     </div>
   )
 }
