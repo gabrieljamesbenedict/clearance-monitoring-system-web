2eef324fe98e1acb27f280a17389099257ec30c4
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-05 05:32:54 +0800

    refactor: use proxy.ts to do an auth check everytime

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,29 +1,45 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import path from 'path';
 
-export function proxy(request: NextRequest) {
-    const cookie = request.cookies.get("JSESSIONID");
+export async function proxy(request: NextRequest) {
     const url = request.nextUrl.clone();
+    const pathname = url.pathname
 
     const publicPaths = ["/login", "/register"];
-    const pathname = request.nextUrl.pathname;
+    const atPublicRoute = publicPaths.some(p => pathname.startsWith(p));
 
-    if (pathname === "/") {
+    const cookieHeader = request.headers.get("cookie") ?? "";
+
+    const res = await fetch("http://localhost:8080/api/auth/me", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            cookie: cookieHeader,
+        }
+    });
+
+    const user = res.json();
+
+    console.log(JSON.stringify(user));
+
+    if (atPublicRoute && res.ok) {
         url.pathname = "/dashboard";
         return NextResponse.redirect(url);
     }
 
-    if (publicPaths.some(path => pathname.startsWith(path))) {
-        if (cookie) {
-            url.pathname = "/dashboard";
-            return NextResponse.redirect(url);
-        }
+    if (!atPublicRoute && !res.ok) {
+        url.pathname = "/login";
+        return NextResponse.redirect(url);
+    }
+
+    if (url.pathname === "/") {
+        url.pathname = "/dashboard";
+        return NextResponse.redirect(url);
     }
 
     return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/:path*",
+  matcher: ["/((?!_next|favicon.ico).*)"]
 };
