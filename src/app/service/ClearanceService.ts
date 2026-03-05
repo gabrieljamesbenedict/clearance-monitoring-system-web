9cabf1fef2c38fcb938b3c62bf0144c320e86ce2
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-05 11:32:54 +0800

    feat: add status field for Clearance interface

---
+++ src/app/service/ClearanceService.ts
@@ -0,0 +1,38 @@
+export interface Clearance {
+    clearanceId: number;
+    userId?: number;
+    purpose: string;
+    academicYear: string;
+    semester: string;
+    status: string;
+    createAt: string;
+    deletedAt?: string;
+}
+
+const getAllStudentClearance = async (): Promise<Clearance[]> => {
+    const res = await fetch(`http://localhost:8080/api/clearances`, {
+        method: "GET",
+        credentials: "include",
+        headers: {"Content-Type": "application/json"}
+    });
+
+    if (!res.ok) {
+        throw new Error(`Request failed: ${res.status}`);
+    }
+
+    return await res.json();
+}
+
+const getStudentClearance = async (id: number): Promise<Clearance> => {
+    const res = await fetch(`http://localhost:8080/api/clearances/${id}`, {
+        method: "GET",
+        credentials: "include",
+        headers: {"Content-Type": "application/json"}
+    });
+
+    if (!res.ok) {
+        throw new Error(`Request failed: ${res.status}`);
+    }
+
+    return await res.json();
+}
