51c43dbde25be0a6c172c28fba8ea4027d371994
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-06 10:36:07 +0800

    style: make button lighter in color when inactive

--- src/app/components/PrimaryButton.tsx
+++ src/app/components/PrimaryButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const PrimaryButton = ({children, active = true, onClick}: any) => {
-  const styling = (active ? "bg-primary-hover hover:-translate-y-1 transition cursor-pointer " : "bg-gray-400 ") + "text-white text-center px-6 py-3 rounded-2xl";
+  const styling = (active ? "bg-primary-hover hover:-translate-y-1 transition cursor-pointer" : "bg-gray-300") + " text-white text-center px-6 py-3 rounded-2xl";
   return (
     <div 
       className={styling}
